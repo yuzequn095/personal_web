508f451e2fa900ec038efa45ecd88c3f1769db46
Zequn Yu <zey031@ucsd.edu>
2018-09-18 13:07:55 -0700

    html+css 1st demo

---
+++ html/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+
+<html>
+
+        <head>
+			
+
+            <title> Home - Zequn Yu</title>
+
+			
+          	<meta charset="utf-8" />
+          	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+          	<meta name="viewport" content="width-device-width, initial-scale-1" />
+                                                                                
+          
+        </head>
+
+
+
+
+    <body>
+    
+    
+        <h1>Nice to meet you here!</h1>
+        <h3><i>zequn's personal website, since 2018</i></h3>
+        
+        
+        <ul>
+            <li><a href="www.zeqyu.com">Home</a></li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Others</li>
+        </ul>
+        
+        <hr>
+        <p><a href="https://www.linkedin.com/in/jason-yu-9593a8107/"> My Linkein - Jason Yu </a></p>
+        <p><a href="https://github.com/yuzequn095"> My Github - yuzequn095</a> </p>
+        
+    
+    </body>
+
+
+
+    
+</html>
